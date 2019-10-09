9982a15f647298aecaedb8d1119c0e9b97ccd2c5
Ваше Имя <you@example.com>
2019-10-09 10:25:07 +0300

    удалил лишние файлы , прочистил код

--- src/dbConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host     : 'localhost',
-  user     : 'smedov',
-  password : 'Volik2017',
-  db      : 'AddelDB',
-  charset : 'utf8mb4'
-};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const getFiles = require('./lib')
 
-let result = getFiles('/home/smedov/Work/Test-Folder-Files/',{debug:true})
+let result = getFiles('/home/smedov/Work/Test_Files/Test_Folder_Files/',{debug:true})
 
-console.log('файлы по дирректории: \n', result.receivedDataFilesPath) //отобразим массив путей c файлами
-console.log('\nСумма во всех файлах по дирректории: ',  result.allSum)
+console.log('файлы по дирректории: \n', result.receivedDataFilesPath) // Отобразим массив путей c файлами
+console.log('\nСумма во всех файлах по дирректории: ',  result.allSum) // Отобразим полученную сумму в файлах
